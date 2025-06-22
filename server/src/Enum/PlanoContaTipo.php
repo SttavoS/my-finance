865750a779d5c7f4be8fb82ae61b70d01c31e3b8
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-06-21 22:05:29 -0300

    map string to Enum

--- server/src/Enum/PlanoContaTipo.php
+++ server/src/Enum/PlanoContaTipo.php
@@ -6,4 +6,9 @@ enum PlanoContaTipo: string
 {
     case Despesa = 'D';
     case Receita = 'R';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
