c9291e1fec918dd0ecc403b8ada31dbf066974a3
Gustavo Schneider <gustavo.schneider.dev@gmail.com>
2025-06-29 02:53:15 -0300

    add PlanoConta DataTable

--- web/src/views/PlanoContas.vue
+++ web/src/views/PlanoContas.vue
@@ -1,7 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFetch } from '@/composables/useFetch.ts';
+import { onMounted, ref } from 'vue';
+import { Button, ButtonGroup, Column, DataTable } from 'primevue';
+
+const request = useFetch();
+const planosConta = ref([]);
+const loading = ref(false);
+const error = ref<string | null>(null);
+
+const fetchPlanosConta = async () => {
+  loading.value = true;
+  try {
+    planosConta.value = await request.GET('http://localhost:8000/api/planos-conta');
+  } catch (error: any) {
+    error.value = error.message;
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => fetchPlanosConta());
+</script>
 
 <template>
   <h1>Plano Contas</h1>
+
+  <div v-if="loading">
+    <ProgressSpinner />
+  </div>
+
+  <div v-if="error" class="error-msg">
+    {{ error }}
+  </div>
+
+  <DataTable :value="planosConta" tableStyle="min-width: 50rem">
+    <Column field="id" header="Código"></Column>
+    <Column field="descricao" header="Descrição"></Column>
+    <Column field="tipo" header="Tipo"></Column>
+    <Column header="Actions">
+      <template #body>
+        <ButtonGroup>
+          <Button label="Editar" icon="pi pi-pencil" />
+          <Button label="Deletar" icon="pi pi-trash" />
+        </ButtonGroup>
+      </template>
+    </Column>
+  </DataTable>
 </template>
 
-<style scoped></style>
+<style scoped>
+.error-msg {
+  color: red;
+}
+</style>
